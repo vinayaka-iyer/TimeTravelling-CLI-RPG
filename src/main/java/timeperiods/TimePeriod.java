5e6017a57ec6122100a8c97480a3471b7f9af256
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2025-02-06 13:28:08 +0530

    Created abstract Timeperiod class - so all timeperiods implement encounterEvent()

--- src/main/java/timeperiods/TimePeriod.java
+++ src/main/java/timeperiods/TimePeriod.java
@@ -1,4 +1,15 @@
 package timeperiods;
 
-public class TimePeriod {
+public abstract class TimePeriod {
+    private String name;
+
+    public TimePeriod(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void encounterEvent();
 }
