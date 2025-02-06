2d371c6178afb4f0c725ddda37e5e15ddc29c96c
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2025-02-06 13:26:18 +0530

    Create TimeTraveller - player class to interact with TimePeriod

--- src/main/java/game/TimeTraveller.java
+++ src/main/java/game/TimeTraveller.java
@@ -1,4 +1,30 @@
 package game;
 
+import timeperiods.TimePeriod;
+
 public class TimeTraveller {
+    private String name;
+    private int health;
+
+    public TimeTraveller(String name) {
+        this.name = name;
+        this.health = 100;
+    }
+
+    public void travelTo(TimePeriod Period) {
+        System.out.println("\n✨ You have traveled to " + period.getName());
+        period.encounterEvent();
+    }
+
+    public void takeDamage(int damage) {
+        health -= damange;
+        if (health <=0) {
+            System.out.println("💀 Oh no! You've died in a time paradox!");
+            System.exit(0);
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
 }
